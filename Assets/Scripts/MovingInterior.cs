3e961177ebb7fd9a5d15139e4750e369554f9e8e
feesh <feesh.git@gmail.com>
2018-08-13 22:42:16 -0400

    Add moving interiors

---
+++ Assets/Scripts/MovingInterior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingInterior : MonoBehaviour {
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Debug.Log("Staying");
+        PlayerController player = other.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            //Debug.Log("updating timer");
+            player.StayInMovingInterior();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("exiting");
+        /*PlayerController player = other.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            player.ExitMovingInterior();
+        }*/
+    }
+}
