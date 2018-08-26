684fb1e184b6f4efd3e877b497a97c87068e16d7
feesh <feesh.git@gmail.com>
2018-08-25 22:34:28 -0400

    Update scene

--- Assets/Scripts/RotatingCollider.cs
+++ Assets/Scripts/RotatingCollider.cs
@@ -18,9 +18,7 @@ public class RotatingCollider : MovingCollider {
         if (player != null) {
             player_pos = player.transform.position - transform.position;
         }
-        Debug.DrawRay(transform.position, player_pos, Color.green);
         Vector3 rotating_velocity = Vector3.Cross(transform.TransformDirection(AngularVelocity), player_pos)*Mathf.Deg2Rad;
-        Debug.DrawRay(transform.position, rotating_velocity, Color.blue);
         return velocity + rotating_velocity;
     }
 }
