29da5e9de30c9e81df2b5087f7bac88f4acc24e8
feesh <feesh.git@gmail.com>
2018-12-15 00:06:09 -0500

    Add host button

--- Assets/Scripts/Networking/TestNetworking.cs
+++ Assets/Scripts/Networking/TestNetworking.cs
@@ -6,33 +6,37 @@ using UnityEngine.UI;
 using MLAPI;
 
 public class TestNetworking : NetworkedBehaviour {
-    public bool makemehost;
     public GameObject MapPrefab;
 
     void Start() {
-        if (makemehost) {
-            NetworkingManager.singleton.StartHost();
-            Debug.Log("Starting map...");
-            GameObject Map = Instantiate(MapPrefab);
-            Map.transform.position = new Vector3(30f, 0, 0);
-            Map.GetComponent<NetworkedObject>().Spawn();
 
-            Destroy(transform.parent.gameObject, 1f);
-            Destroy(gameObject);
-        }
-        var input = gameObject.GetComponent<InputField>();
-        var se = new InputField.SubmitEvent();
+        InputField input = GetComponent<InputField>();
+        InputField.SubmitEvent se = new InputField.SubmitEvent();
         se.AddListener(SubmitName);
         input.onEndEdit = se;
+
+        UnityEngine.UI.Button button = transform.parent.GetComponentInChildren<UnityEngine.UI.Button>();
+        button.onClick.AddListener(StartHost);
 
         //or simply use the line below, 
         //input.onEndEdit.AddListener(SubmitName);  // This also works
     }
 
+    private void StartHost() {
+        NetworkingManager.singleton.StartHost();
+        Debug.Log("Starting map...");
+        GameObject Map = Instantiate(MapPrefab);
+        Map.transform.position = new Vector3(30f, 0, 0);
+        Map.GetComponent<NetworkedObject>().Spawn();
+
+        Destroy(transform.parent.gameObject, 0.1f);
+        Destroy(gameObject);
+    }
+
     private void SubmitName(string ip) {
         NetworkingManager.singleton.NetworkConfig.ConnectAddress = ip;
         NetworkingManager.singleton.StartClient();
-        Destroy(transform.parent.gameObject, 1f);
+        Destroy(transform.parent.gameObject, 0.1f);
         Destroy(gameObject);
     }
 }
