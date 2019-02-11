676ab022c0d2103870430448076d185dec14538b
feesh <feesh.git@gmail.com>
2019-02-10 21:01:34 -0500

    Add IOProximityDetector

---
+++ Assets/Scripts/Entities/IOEntities/IOEntities/IOProximityDetector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[AddComponentMenu("IOEntities/IOProximityDetector")]
+[RequireComponent(typeof(Collider))]
+public class IOProximityDetector : IOEntity {
+    public DigitalState Detected;
+    public float range = 10f;
+    private string DETECTION_TIMER;
+
+    protected override void Awake() {
+        base.Awake();
+        DETECTION_TIMER = $"ProximityDetection_{GetInstanceID()}";
+        utils.CreateTimer(DETECTION_TIMER, 0.1f).setFinished();
+    }
+
+    private void FixedUpdate() {
+        if (!isServer) return;
+        Detected.state = !utils.CheckTimer(DETECTION_TIMER);
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (!isServer) return;
+        if (!other.GetComponent<MovingPlayer>()) return;
+        if ((other.transform.position - transform.position).magnitude < range) {
+            utils.ResetTimer(DETECTION_TIMER);
+        }
+    }
+}
